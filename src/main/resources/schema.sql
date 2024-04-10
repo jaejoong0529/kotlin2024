7b5de9b48fcd1698668144ba5fab568b3d5f5314
Jacob <w0nse0k@naver.com>
2024-04-11 05:19:15 +0900

    주석 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+-- JDBC에서 사용하는 테이블들
+-- 테이블명을 소문자로 쓰고, 컬럼명에 underscore case를 사용한다.
 CREATE TABLE if not exists user (
   id          int         NOT NULL AUTO_INCREMENT,
   username    varchar(50) NOT NULL,
@@ -22,6 +24,8 @@ CREATE TABLE if not exists post (
   FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
+-- JPA에서 사용하는 테이블들.
+-- 테이블명이 대문자로 시작하고, 컬럼명에 camel case를 사용한다.
 CREATE TABLE if not exists User (
   id         bigint      NOT NULL AUTO_INCREMENT,
   username   varchar(50) NOT NULL,
@@ -37,7 +41,7 @@ CREATE TABLE if not exists Post (
   id           bigint       NOT NULL AUTO_INCREMENT,
   title        varchar(255) NOT NULL,
   content      text         NOT NULL,
-  user_id      bigint       NOT NULL,
+  user_id      bigint       NOT NULL, -- User.id를 참조하는 foreign key
   pubDate      datetime     NOT NULL DEFAULT current_timestamp(),
   lastModified timestamp    NOT NULL DEFAULT current_timestamp() on update current_timestamp(),
   PRIMARY KEY (id),
