7de7f6e6f46ce64047504961516642bdcf0f0cdd
Jacob <w0nse0k@naver.com>
2024-05-01 03:45:06 +0900

    @Import로 수정

--- src/main/kotlin/kr/mjc/jacob/thymeleaf/ThymeleafConfig.kt
+++ src/main/kotlin/kr/mjc/jacob/thymeleaf/ThymeleafConfig.kt
@@ -3,14 +3,16 @@ package kr.mjc.jacob.thymeleaf
 import kr.mjc.jacob.spring.jpa.JpaConfig
 import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.templatemode.TemplateMode
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
 
-/** 템플릿 엔진을 빈으로 구성한다. */
-@ComponentScan(basePackageClasses = [JpaConfig::class])
+@Configuration
+@Import(JpaConfig::class)
 open class ThymeleafConfig {
+  /** 템플릿 엔진을 빈으로 구성한다. */
   @Bean
   open fun templateEngine() = TemplateEngine().apply {
     setTemplateResolver(ClassLoaderTemplateResolver().apply {
@@ -23,3 +25,9 @@ open class ThymeleafConfig {
 
 val applicationContext =
   AnnotationConfigApplicationContext(ThymeleafConfig::class.java)
+
+fun main() {
+  applicationContext.beanDefinitionNames.forEachIndexed { i, name ->
+    println("$i : $name")
+  }
+}
